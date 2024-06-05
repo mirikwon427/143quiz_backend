da3ad5c0e329dd0ba613ffede0ef1e040de00120
Jeong Jihwan <wlghks0461@gmail.com>
2024-06-05 20:13:09 +0900

    [GB-5] feat: create Rating Entity without relationship

---
+++ src/main/java/garlicbears/_quiz/domain/Rating.java
@@ -0,0 +1,25 @@
+package garlicbears._quiz.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rating")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ratingId;
+
+    //  User
+
+    @Column(name = "rating_value", nullable = false)
+    @ColumnDefault("5")
+    private Double ratingValue;
+
+    @Column(name = "rating_at", nullable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    private LocalDateTime ratingDate;
+
+}
