76fb3296a1030498fa36cc77a8d1c09bc7af4bb8
Jeong Jihwan <wlghks0461@gmail.com>
2024-06-05 20:21:07 +0900

    [GB-5] feat: create Admin, Role Entity without relationship

---
+++ src/main/java/garlicbears/_quiz/domain/Admin.java
@@ -0,0 +1,21 @@
+package garlicbears._quiz.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_seq")
+    private Long adminId;
+
+    @Column(name = "admin_email", nullable = false, unique = true, length = 200)
+    private String adminEmail;
+
+    @Column(name = "admin_password", nullable = false, length = 100)
+    private String adminPassword;
+
+    //  BaseTimeEntity
+    //  Active
+}
---
+++ src/main/java/garlicbears/_quiz/domain/Role.java
@@ -0,0 +1,18 @@
+package garlicbears._quiz.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_seq")
+    private Long roleId;
+
+    @Column(name = "role_name", nullable = false)
+    private String roleName;
+
+    //  BaseTimeEntity
+
+}
