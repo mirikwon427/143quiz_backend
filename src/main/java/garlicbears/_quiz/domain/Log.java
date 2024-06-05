191243ab9499ec0f6bb66291ab3f4ac8ac7413b6
Jeong Jihwan <wlghks0461@gmail.com>
2024-06-05 20:26:17 +0900

    [GB-5] feat: create Log Entity without relationship

---
+++ src/main/java/garlicbears/_quiz/domain/Log.java
@@ -0,0 +1,30 @@
+package garlicbears._quiz.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "logs")
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_seq")
+    private Long logId;
+
+    //  User
+
+    @Column(name = "ip_address", nullable = false)
+    private String ipAddress;
+
+//    @Column(name = "user_agent", nullable = false)
+//    private String userAgent;
+
+    @Column(name = "log_url", nullable = false)
+    private String url;
+
+    @Column(name = "log_created_at", nullable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+}
