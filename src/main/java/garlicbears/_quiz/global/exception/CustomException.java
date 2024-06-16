2a1b139875bfe4b34a552111055f6c637d2fb3b2
mirikwon427 <mirikwon2719@gmail.com>
2024-06-16 20:44:44 +0900

    GB-13 refactor: remove lombok, create constructor, getter, setter

--- src/main/java/garlicbears/_quiz/global/exception/CustomException.java
+++ src/main/java/garlicbears/_quiz/global/exception/CustomException.java
@@ -1,14 +1,15 @@
 package garlicbears._quiz.global.exception;
 
-import lombok.Getter;
-
-@Getter
 public class CustomException extends RuntimeException{
     private final ErrorCode errorCode;
 
     public CustomException(ErrorCode errorCode) {
        super(errorCode.getMessage());
        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode(){
+        return errorCode;
     }
 
     public int getStatus() {
